7264b96dc9de1eb49ed6cd48fdb5936ee6f341c1
juanalfonso27 <juanv27072017@gmail.com>
2025-10-08 11:57:35 -0300

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -3,7 +3,6 @@
 
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js'
 import { getAnalytics } from 'https://www.gstatic.com/firebasejs/9.22.2/firebase-analytics.js'
-import { getFirestore, enableOfflinePersistence } from 'https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js'
 import { getAuth } from 'https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js'
 import { getStorage } from 'https://www.gstatic.com/firebasejs/9.22.2/firebase-storage.js'
 
